44ec79d62b0abb307c942cfcc0e213ca506c07dd
Kartik Bhatia <37398223+KnightKnight27@users.noreply.github.com>
2023-11-07 14:31:07 +0530

    Update SQL-6-Joins-Cntd.sql

--- SQL-6-Joins-Cntd.sql
+++ SQL-6-Joins-Cntd.sql
@@ -6,5 +6,36 @@ select a.title,l.name
 select * from film cross join language;
 
 
+select *
+from film_actor fa
+join film f
+on fa.film_id = f.film_id;
 
+
+select *
+from film_actor fa
+left join film f
+on fa.film_id = f.film_id;
+
+
+select *
+from film_actor fa
+right join film f
+on fa.film_id = f.film_id;
+#
+# select *
+# from film_actor fa
+# full outer join film f
+# on fa.film_id = f.film_id;
+
+
+-- No support of full join in MySQL
+
+
+select *
+from film cross join language;
+
+
+select film.film_id, film_actor.actor_id, title
+from film_actor
 
